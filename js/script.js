aac7ad1e909f126903862fc112af9ddd3efea6a4
sszahinos <sergiosaza6@gmail.com>
2021-11-10 20:10:57 +0100

    improved buttons and drawing. now detects only when clicking. implemented buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+const ROOT = document.documentElement;
 const BUT_ERASE = document.querySelector("#but-erase");
 const BUT_COLOR = document.querySelector("#but-color");
 const BUT_RAINBOW = document.querySelector("#but-rainbow");
+const BUT_CLEAR = document.querySelector("#but-clear");
 
 const CONTAINER = document.querySelector(".grid-container");
 const CELL = document.createElement("div");
@@ -22,16 +24,20 @@ fillGrid(10);
 function configureButtons() {
     BUT_COLOR.addEventListener( 
         "click",
-        (e) => {mode = "colored";console.log(mode);}
+        (e) => {mode = "colored";}
     );
     BUT_RAINBOW.addEventListener( 
         "click",
-        (e) => {mode = "rainbow";console.log(mode);}
+        (e) => {mode = "rainbow";}
     );
     BUT_ERASE.addEventListener( 
         "click",
-        (e) => {mode = "erased";console.log(mode);}
-    );    
+        (e) => {mode = "erased";}
+    );   
+    BUT_CLEAR.addEventListener(
+        "click",
+        (e) => {clearColors()}
+    );
 }
 
 //Creates a square grid with gridNum HxV
@@ -39,7 +45,6 @@ function fillGrid(gridNum) {
     CONTAINER.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
     for (let row = 0; row < gridNum; row++) {
         for (let col = 0; col < gridNum; col++) {
-            //console.log("hola");
             CONTAINER.appendChild(CELL.cloneNode(true));      
         }   
     }
@@ -53,22 +58,42 @@ function addHoverListener() {
 
     cells.forEach(cell => {
         cell.addEventListener("mouseenter", (e) => {
-            console.log("entered: " + cell);
-            //e.target.style.backgroundColor = selectedColor;
+            if (e.buttons == 1) { //If clicked
+                colorCell(cell);
+            }
+        });
+        cell.addEventListener("touchmove", (e) => {
+            console.log("event: " + e);        
             colorCell(cell);
+            
         });
     });
 }
 
 function colorCell(cell) {
-    if (!cell.classList.contains(`${mode}`)) {
+    if (!cell.classList.contains(`${mode}`) || cell.classList.contains(MODES[0])) {
         resetCellColor(cell);
         cell.classList.toggle(`${mode}`);
+        console.log("Mode: " + mode);
+        if (mode !== MODES[2]) { //If is not erasing mode
+            cell.style.backgroundColor = BUT_COLOR.value;
+        } else {
+            cell.style.removeProperty('background-color');
+        }
+        
     }
 }
 
 function resetCellColor(cell){
     cell.classList = "grid-item";
+}
+
+function clearColors() {
+    let cells = document.querySelectorAll(".grid-item");
+    cells.forEach(cell => {
+        cell.style.removeProperty('background-color');
+        cell.className = "grid-item";
+    });
 }
 
 /*
